a2b33fc0810277eb0bedf8750d4ed988c5251de1
ezefferth <ezefferth96@gmail.com>
2023-05-17 22:48:10 -0300

    crud

--- src/controllers/AlunoCreateController.ts
+++ src/controllers/AlunoCreateController.ts
@@ -22,9 +22,22 @@ export class FindAlunoController {
   async handle(request: Request, response: Response) {
     //const { nome } = request.body;
     try {
-      const aluno = await prismaClient.aluno.findMany({
-        where: {
-          nome: "Mikael"
+      const aluno = await prismaClient.aluno.findMany({});
+      return response.json(aluno);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: "Erro ao cadastrar aluno" });
+    }
+  }
+}
+
+export class DeleteAlunoController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.body;
+    try {
+      const aluno = await prismaClient.aluno.delete({
+        where : {
+          id: id
         }
       });
       return response.json(aluno);
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,30 @@
 import express from 'express';
 import { router } from './routes';
-import { CreateAlunoController, FindAlunoController } from './controllers/AlunoCreateController';
-
+import { CreateAlunoController, DeleteAlunoController, FindAlunoController } from './controllers/AlunoCreateController';
+import cors from 'cors'
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
+
 const createAluno = new CreateAlunoController()
 const findAluno = new FindAlunoController();
+const deleteAluno = new DeleteAlunoController();
 
-router.post("/createAluno", createAluno.handle)
 router.get("/findAluno", findAluno.handle)
-
+router.post("/createAluno", createAluno.handle)
+router.delete("/deleteAluno", deleteAluno.handle )
 
 app.listen(4003, () => console.log("server is running on PORT 4003"));
+
+
+
+
+
+
+
+
+
 
